87ccc9600a9ff9ee6db582823e5832c58156108b
yuran4ik <shlyapin.y.a@gmail.com>
2025-09-22 13:52:38 +0400

    add test project

---
+++ docker_test/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    res.send("asd")
+})
+
+app.listen(8080, 'localhost', (err) => {
+    console.log('http://localhost:8080/')
+})
